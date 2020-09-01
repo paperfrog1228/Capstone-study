0a3e733d720481742e1307be75f68fed29ac4f6f
paperfrog1228 <paperfrog@naver.com>
2020-09-01 16:33:04 +0900

    Add player and camera mover 현실도피 세상 끝까지 하다 이제 알바와서 코딩하는 지경에 다다랐다.

---
+++ Unity_Dnon/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    void Update()
+    {
+        Vector3 pos=player.transform.position;
+        gameObject.transform.position=new Vector3(pos.x,pos.y,pos.z-10f);
+    }
+}
---
+++ Unity_Dnon/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Mover : MonoBehaviour
+{
+    [SerializeField,Range(0,30)]
+    private float speed=5.0f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontal, Space.World);
+        transform.Translate(Vector3.up  * Time.deltaTime * speed * vertical, Space.World);
+    }
+}
